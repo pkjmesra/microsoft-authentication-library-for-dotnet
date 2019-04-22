95a3ea92b40cd8528f67187aac543a6427e77b2d
Jenny Ferries <jeferrie@microsoft.com>
2019-04-22 11:22:25 -0700

    ios

--- tests/Microsoft.Identity.Test.Core.UIAutomation/MobileTestHelper.cs
+++ tests/Microsoft.Identity.Test.Core.UIAutomation/MobileTestHelper.cs
@@ -246,8 +246,9 @@ namespace Microsoft.Identity.Test.UIAutomation.Infrastructure
                 controller.Application.Tap(x => x.Id(CoreUiTestConstants.AuthorityPickerId));
                 Console.WriteLine("open the UI picker");
                 Console.WriteLine("On iOS device, select the {0} authority", authority);
-                controller.Application.WaitForElement(x => x.Class(CoreUiTestConstants.AuthorityPickerId));
-                controller.Application.Query(x => x.Class(CoreUiTestConstants.AuthorityPickerId).Invoke("selectRow", authority, "inComponent", 1, "animated", true));
+                controller.Application.Tap(x => x.Marked(authority));
+                controller.Application.WaitForElement(x => x.Class("Picker"));
+                controller.Application.Query(x => x.Class("Picker").Invoke("selectRow", authority, "inComponent", 1, "animated", true));
             }
             else
             {
